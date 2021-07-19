eae8e2b7a3e9cfb117d043def1a9440c80eba786
Kvazar3S273 <ba2h007@gmail.com>
2021-07-19 23:54:05 +0300

    first push

---
+++ WebAppSite/WebAppSite/Controllers/AnimalController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppSite.Models;
+
+namespace WebAppSite.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<AnimalsViewModel> model = new List<AnimalsViewModel>();
+            model.Add(new AnimalsViewModel
+            {
+                Id=1,
+                BirthDay=DateTime.Now,
+                Price=200,
+                Name="Ондатра Сара",
+                Image="1.jpg"
+            });
+            model.Add(new AnimalsViewModel
+            {
+                Id = 2,
+                BirthDay = DateTime.Now,
+                Price = 200,
+                Name = "Павіван Бора",
+                Image = "2.jpg"
+            });
+            model.Add(new AnimalsViewModel
+            {
+                Id = 3,
+                BirthDay = DateTime.Now,
+                Price = 200,
+                Name = "Павіван Жора",
+                Image = "3.jpg"
+            });
+            return View(model);
+        }
+    }
+}
