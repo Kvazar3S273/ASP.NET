7bb6e55c5a1d69d3a3bdebe45c498c4852a5b857
Kvazar3S273 <ba2h007@gmail.com>
2021-09-03 00:11:19 +0300

    add table users to view

--- WebAppSite/WebAppSite/Areas/Admin/Controllers/DashboardsController.cs
+++ WebAppSite/WebAppSite/Areas/Admin/Controllers/DashboardsController.cs
@@ -1,19 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppSite.Areas.Admin.Models;
+using WebAppSite.Domain;
+using WebAppSite.Domain.Entities.Identity;
 
 namespace WebAppSite.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class DashboardsController : Controller
     {
-
-        public IActionResult Dashboard_0()
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppEFContext _context;
+        public AppRole role { get; set; }
+        public DashboardsController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, AppEFContext context)
         {
-            return View();
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
         }
+        public async Task<IActionResult> Dashboard_0()
+        {
+            var userRole = (from user in _context.Users
+                                  select new{
+                                      UserId = user.Id,
+                                      Username = user.UserName,
+                                      Email = user.Email,
+                                      RoleNames = (from userRole in user.UserRoles
+                                                   join role in _context.Roles on userRole.RoleId
+                                                   equals role.Id
+                                                   select role.Name).ToList()})
+                                                   .ToList()
+                                                   .Select(p => new RolesViewModel(){
+                                      UserId = p.UserId,
+                                      Username = p.Username,
+                                      Email = p.Email,
+                                      Role = string.Join(",", p.RoleNames)
+                                  });
+
+            return await Task.FromResult(View(userRole));
+        }
+
+        //[HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Dashboard_0");
+                else
+                    ModelState.AddModelError("", "Не можливо видалити користувача");
+            }
+            else
+                ModelState.AddModelError("", "Не вдалося знайти користувача");
+
+            return View("Dashboard_0", _userManager.Users);
+        }
+
         public IActionResult Dashboard_1()
         {
             return View();
---
+++ WebAppSite/WebAppSite/Areas/Admin/Models/RolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppSite.Areas.Admin.Models
+{
+    public class RolesViewModel
+    {
+        public long UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
